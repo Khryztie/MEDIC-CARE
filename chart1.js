c30e81e3faa744b2d55f976318f02822a96b1c21
Khryztie <ekechristiana@gmail.com>
2024-04-13 12:24:31 +0100

    Added new pages

---
+++ chart1.js
@@ -0,0 +1,21 @@
+var ctx = document.getElementById('lineChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'],
+        datasets: [{
+            label: 'Days of the week',
+            data: [0, 10, 20, 30, 0, 50],
+            backgroundColor: [
+                'rgba(85,85,85, 1)'
+
+            ],
+            borderColor: 'rgb(41, 155, 99)',
+
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true
+    }
+});
---
+++ chart2.js
@@ -0,0 +1,34 @@
+
+var ctx2 = document.getElementById('doughnut').getContext('2d');
+var myChart2 = new Chart(ctx2, {
+    type: 'doughnut',
+    data: {
+        labels: ['Profile', 'Medical history', 'Medical upload', 'Account setting', 'Health education'],
+
+        datasets: [{
+            label: 'Patient Analytics',
+            data: [20, 12, 8, 6],
+            backgroundColor: [
+                'rgba(41, 155, 99, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(120, 46, 139,1)',
+                'rgba(300, 45, 15,1)'
+
+            ],
+            borderColor: [
+                'rgba(41, 155, 99, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(120, 46, 139,1)'
+
+            ],
+            borderWidth: 1
+            
+        }]
+
+    },
+    options: {
+        responsive: true
+    }
+});
